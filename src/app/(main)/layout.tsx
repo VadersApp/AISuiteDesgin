ab6d3a4182170d5eb4373b827df3a761d3f6dff4
Kevin Willaschek <kw@igg-gmbh.de>
2026-01-20 13:51:42 +0000

    erhöhe den abstand wie der Abstand zum rechten rand, hier ist der Abstan

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -12,7 +12,7 @@ export default function MainLayout({ children }: PropsWithChildren) {
       </Sidebar>
       <SidebarInset className="relative bg-slate-50 dark:bg-[radial-gradient(circle_at_top_right,_var(--tw-gradient-stops))] dark:from-slate-900 dark:via-slate-950 dark:to-background flex flex-col flex-1">
         <Header />
-        <div className="flex-1 overflow-y-auto p-10 custom-scrollbar">
+        <div className="flex-1 overflow-y-auto p-12 custom-scrollbar">
             <div className="max-w-7xl mx-auto pb-20">
                 {children}
             </div>
