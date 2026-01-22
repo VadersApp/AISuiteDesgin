1d68231c546078fba45e31bef718ccf6988426d5
Kevin Willaschek <kw@igg-gmbh.de>
2026-01-22 13:29:13 +0000

    Verwende im Darcmode diese Farbwerte für den Verlauf der Hintergundfarbe

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,7 +7,7 @@ import { Header } from '@/components/header';
 export default function MainLayout({ children }: PropsWithChildren) {
   return (
     <SidebarProvider>
-      <Sidebar className="w-72 border-r-0 bg-gradient-to-b from-slate-800 to-slate-900 flex flex-col transition-all duration-300 z-50 flex-shrink-0">
+      <Sidebar className="w-72 border-r-0 bg-sidebar dark:bg-[linear-gradient(90deg,#0E1A27_0%,#0E1622_50%,#101522_100%)] flex flex-col transition-all duration-300 z-50 flex-shrink-0">
         <MainNav />
       </Sidebar>
       <SidebarInset className="relative bg-slate-50 dark:bg-[radial-gradient(circle_at_top_left,_var(--tw-gradient-stops))] dark:from-[#12233A] dark:to-[#080E1A] flex flex-col flex-1">
