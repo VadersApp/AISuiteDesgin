6d5d9ae69a3c4d13772a0cb5a7b0f79276e1f4bb
Kevin Willaschek <kw@igg-gmbh.de>
2026-02-04 05:51:46 +0000

    Try fixing this error: `Runtime ReferenceError: MessageSquare is not def

--- src/app/(main)/qalender/page.tsx
+++ src/app/(main)/qalender/page.tsx
@@ -24,6 +24,7 @@ import {
   BrainCircuit,
   Mail,
   Send,
+  MessageSquare,
 } from 'lucide-react';
 import { Switch } from '@/components/ui/switch';
 import { Label } from '@/components/ui/label';
